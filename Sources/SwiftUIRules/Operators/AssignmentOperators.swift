bdc682188078d7fd6ddd3ce0178f0b64007ae12a
Helge Heß <devteam@zeezide.de>
2019-09-03 14:29:44 +0200

    Autobox View assignments in AnyView
    When the key expects an AnyView and the right side
    provides some concrete View, the `<=` operator now
    autoboxes the view in an AnyView.

--- Sources/SwiftUIRules/Operators/AssignmentOperators.swift
+++ Sources/SwiftUIRules/Operators/AssignmentOperators.swift
@@ -5,19 +5,32 @@
 //  Created by Helge Heß on 01.09.19.
 //
 
+import protocol SwiftUI.View
+import struct   SwiftUI.AnyView
+
 // \.title <= "hello"
-public
-func <= <Value>(lhs: Swift.KeyPath<RuleContext, Value>, rhs: Value)
-                -> RuleTypeIDAssignment<Value>
+public func <= <Value>(lhs: Swift.KeyPath<RuleContext, Value>, rhs: Value)
+               -> RuleTypeIDAssignment<Value>
 {
   RuleTypeIDAssignment(lhs, rhs)
 }
 
 // \.title <= "hello"
-public
-func <= <Value>(lhs: Swift.KeyPath<RuleContext, Value>,
-                rhs: Swift.KeyPath<RuleContext, Value>)
-                -> RuleTypeIDPathAssignment<Value>
+public func <= <Value>(lhs: Swift.KeyPath<RuleContext, Value>,
+                       rhs: Swift.KeyPath<RuleContext, Value>)
+               -> RuleTypeIDPathAssignment<Value>
 {
   RuleTypeIDPathAssignment(lhs, rhs)
+}
+
+// \.view <= MyView()
+public func <= <V: View>(lhs: Swift.KeyPath<RuleContext, AnyView>, rhs: V)
+               -> RuleTypeIDAssignment<AnyView>
+{
+  RuleTypeIDAssignment(lhs, AnyView(rhs))
+}
+public func <= (lhs: Swift.KeyPath<RuleContext, AnyView>, rhs: AnyView)
+               -> RuleTypeIDAssignment<AnyView>
+{
+  RuleTypeIDAssignment(lhs, rhs)
 }
